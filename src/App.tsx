1e8ac37864e4d9b7954681e18a7995d79061a4aa
danilloele <danillo_uzumaki@hotmail.com.br>
2021-12-01 23:02:47 -0300

    Inclusão dos componentes Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import Login from "./paginas/login/Login";
 import {Grid} from "@material-ui/core";
 import Home from "./paginas/home/Home"
 import './App.css';
@@ -8,9 +11,26 @@ import './App.css';
 function App() {
   return (
    <>
-    <Navbar />
-    <Home />
-    <Footer />
+    <Router>
+      <Navbar/>
+      <Switch>
+        <div style={{minHeight: "100vh"}}>
+          <Route path= "/home">
+            <Home/>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path= "/cadastrousuario">
+            <CadastroUsuario/>
+          </Route>
+        </div>
+      </Switch>
+      <Footer/>
+    </Router>
    </>
   );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,22 +5,22 @@ import "./Home.css";
 function Home(){
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#E8D3B6" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
                 <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "#665544", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#665544", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    <Box paddingX={2} >
+                        <Typography variant="h3" gutterBottom  component="h3" align="center" className="titulo-home">Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom  component="h5" align="center" className="titulo-home">expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "#E8D3B6", backgroundColor: "#665544", color: "#E8D3B6" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className="botao-home">Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className="postagens">
                 </Grid>
             </Grid>
         </>
